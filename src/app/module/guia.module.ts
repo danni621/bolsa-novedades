98439f395ae6ddb778761834e91e0453290c1480
monicab21 <baquero.monica@gmail.com>
2023-08-28 15:14:10 -0500

    ajustes front

--- src/app/module/guia.module.ts
+++ src/app/module/guia.module.ts
@@ -10,4 +10,25 @@ import { CommonModule } from '@angular/common';
 export class GuiaModule {
     guia: string = "";
     cantidadguias: string = "";
+    origen: string = "";
+    destino: string = "";
+    fechaadmision: string ="";
+    peso_envio: string = "";
+    volumen_envio: string = "";
+    largo_envio: string = "";
+    ancho_envio: string = "";
+    alto_envio: string = "";
+    valorcomercial_envio: string = "";
+    valortotal_envio: string = "";
+    fechaAuditoria: string = "";
+    peso_auditoria: string = "";
+    volumen_auditoria: string = "";
+    largo_auditoria: string = "";
+    ancho_auditoria: string = "";
+    alto_auditoria: string = "";
+    valortotal_auditoria: string = "";
+    diferencia_peso: string = "";
+    auditor: string = "";
+    observacion: string = "";
+    valorajuste: string = "";
 }
--- src/app/pages/bolsa-novedades/bolsa-novedades.component.ts
+++ src/app/pages/bolsa-novedades/bolsa-novedades.component.ts
@@ -16,6 +16,27 @@ export class BolsaNovedadesComponent {
   constructor() {
     this.guia.guia = '7000000';
     this.guia.cantidadguias = '99';
+    this.guia.origen = "Medellin";
+    this.guia.destino = "Bogota";
+    this.guia.fechaadmision = "23/08/2023";
+    this.guia.peso_envio = "1";
+    this.guia.volumen_envio = "3";
+    this.guia.largo_envio = "50";
+    this.guia.ancho_envio = "50";
+    this.guia.alto_envio = "50";
+    this.guia.valorcomercial_envio = "25000";
+    this.guia.valortotal_envio = "18500";
+    this.guia.fechaAuditoria = "25/08/2023";
+    this.guia.peso_auditoria = "5";
+    this.guia.volumen_auditoria = "10";
+    this.guia.largo_auditoria = "85";
+    this.guia.ancho_auditoria = "85";
+    this.guia.alto_auditoria = "85";
+    this.guia.diferencia_peso = "7";
+    this.guia.auditor = "Ricardo Quevedo";
+    this.guia.observacion = "N/A";
+    this.guia.valortotal_auditoria = "54000";
+    this.guia.valorajuste = "35500";
   }
 
   ngOnInit(): void {
